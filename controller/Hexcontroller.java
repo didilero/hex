6c6dd456de6f19a33fda9ccc0cada4a7a4aed657
Mathieu <49062886+Midar22@users.noreply.github.com>
2021-05-15 18:14:46 +0200

    Add files via upload

---
+++ controller/Hexcontroller.java
@@ -0,0 +1,14 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class Hexcontroller {
+	@FXML
+	private GridPane grille;
+	
+	@FXML
+	public void initialize() {
+		
+	}
+}
